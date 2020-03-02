684dd45394bf71a44b97642cd6b2b7813fea0c72
ThowV <thomasvisscher@outlook.com>
2020-03-02 13:19:41 +0100

    Fix scaling issues

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import os
 import sys
+
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication
 from src import main_window
 from src.handlers import file_handler, settings_handler, command_handler, converter_handler
@@ -18,7 +21,9 @@ def initialize(main_window_inst):
 
 if __name__ == "__main__":
     # Setup the UI
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
     app = QApplication(sys.argv)
+    app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     # app.setStyle("Fusion")
 
     window = main_window.MainWindow()
